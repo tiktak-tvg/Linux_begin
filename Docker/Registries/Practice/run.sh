8b3474b92c7752c9264f015c79cdf66c9365989f
Vitaly <tfanfan@rambler.ru>
2025-03-15 12:04:36 +0300

    Update run.sh

--- Docker/Registries/Practice/run.sh
+++ Docker/Registries/Practice/run.sh
@@ -10,6 +10,7 @@ docker run --restart always -d --name registry --net school registry:2
 # apt-get update && apt-get install iputils-ping vim curl -y
 
 # https://www.digitalocean.com/community/tutorials/how-to-use-certbot-standalone-mode-to-retrieve-let-s-encrypt-ssl-certificates-on-ubuntu-16-04
+# https://serverspace.io/support/help/how-to-get-lets-encrypt-ssl-on-ubuntu/
 sudo docker container cp /etc/letsencrypt/archive/sotream.xyz/fullchain1.pem proxy:/etc/ssl/certs/fullchain.pem
 sudo docker container cp /etc/letsencrypt/archive/sotream.xyz/privkey1.pem proxy:/etc/ssl/certs/privkey.pem
 sudo docker container cp /etc/letsencrypt/archive/sotream.xyz/chain1.pem proxy:/etc/ssl/certs/chain.pem
